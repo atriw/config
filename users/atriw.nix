b6aab96ed542bcf812cd51e90cb68716e9140df0
atriw <875241499@qq.com>
2023-03-18 02:14:47 +0800

    Try to change user's shell

--- users/atriw.nix
+++ users/atriw.nix
@@ -1,10 +1,11 @@
-{hmUsers, ...}: {
+{hmUsers, pkgs, ...}: {
   home-manager.users = {inherit (hmUsers) atriw;};
 
   users.users.atriw = {
     password = "nixos";
     isNormalUser = true;
     extraGroups = ["wheel"];
+    shell = pkgs.zsh;
   };
 }
 
